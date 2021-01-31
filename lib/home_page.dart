91ca254ae30aa26b82652c678030619946be7fe0
MT13 <maria.tarielashvili@gmail.com>
2021-01-31 16:30:31 +0400

    Added theme and language localisation

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
-
   final String title;
 
   @override
@@ -10,30 +10,113 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _padding = EdgeInsets.all(16.0);
+  final _signInButtonWidth = 32;
 
-  Container _createTopBar(){
-    return Container(
-
+  Widget _createTopBar() {
+    return Padding(
+      padding: _padding,
+      child: Container(
+        color: Theme.of(context).colorScheme.background,
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(
+                        right: 8,
+                      ),
+                      child: Container(
+                        width: 35,
+                        height: 35,
+                        decoration: new BoxDecoration(
+                          color: Colors.red,
+                          shape: BoxShape.circle,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: 8,
+                      ),
+                      child: Placeholder(
+                        fallbackWidth: 35,
+                        fallbackHeight: 36,
+                      ),
+                    ),
+                  ]),
+              Row(children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(right: 8),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(AppLocalizations.of(context).register,
+                        style: TextStyle(color: Colors.white)),
+                    style: TextButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                        side: BorderSide(),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 8),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text(AppLocalizations.of(context).signIn,
+                        style: TextStyle(color: Colors.white)),
+                    style: TextButton.styleFrom(
+                      backgroundColor: Theme.of(context).colorScheme.secondary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                        side: BorderSide(color: Colors.red),
+                      ),
+                    ),
+                  ),
+                ),
+              ]),
+            ]),
+      ),
     );
   }
-  
+
+  Container _createNavBar() {
+    return Container(child: Placeholder());
+  }
+
+  Container _createMainSwipe() {
+    return Container(child: Placeholder());
+  }
+
+  Container _createSwipeList() {
+    return Container(child: Placeholder());
+  }
+
+  Container _createBottomContainer() {
+    return Container(child: Placeholder());
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: SingleChildScrollView(
-        child: Column (
-          children: <Widget>[
+        child: Container(
+          color: Theme.of(context).colorScheme.background,
+          child: Column(children: <Widget>[
             _createTopBar(),
             _createNavBar(),
             _createMainSwipe(),
-            _createSwipesList(),
+            _createSwipeList(),
             _createBottomContainer(),
-          ]
-        )
+          ]),
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'home_page.dart';
 
 void main() {
@@ -6,18 +8,38 @@ void main() {
 }
 
 class EuropeBetMain extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    // Size screenSize = MediaQuery.of(context).size;
+    // Orientation orientation = MediaQuery.of(context).orientation;
+
     return MaterialApp(
       title: 'EuropeBet',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        backgroundColor: Color(0xFF1C1D1E)
+        colorScheme: ColorScheme(
+          background: Color(0xFF1C1D1E),
+          brightness: Brightness.dark,
+          error: Colors.red,
+          onError: Colors.white,
+          onBackground: Colors.white,
+          onPrimary: Colors.white,
+          onSecondary: Colors.white,
+          primary: Color(0xFF1C1D1E),
+          primaryVariant: Color(0xFF1C1D1E),
+          secondary: Color(0xFFEF602A),
+          secondaryVariant: Color(0xFFF05A22),
+          surface: Color(0xFF1C1D1E),
+          onSurface: Colors.white,
+        ),
+
+        fontFamily: 'Helvetica',
+        textTheme: TextTheme(
+          button: TextStyle(fontSize: 12),
+        )
       ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
       home: HomePage(title: 'EuropeBet'),
     );
   }
 }
-
